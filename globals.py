cef345448f537e5bfaf06be1b36bbc96e6d2ade5
Margulis162 <markmargulis@proton.me>
2024-06-14 04:08:08 -0400

    user initial input fixed

--- globals.py
+++ globals.py
@@ -1,2 +1,7 @@
+import os 
+# f(x)_________________________________________________________________
+def clean():
+    '''console cleaner'''
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 score = 0
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import os
+import globals
 from colorama import init, Fore, Back, Style
 
-# _______f(x)____________________________________________________________________________________________________________________
+# _______f(x)____
 def list_files():
-    '''makes a list of avalible json files for study'''
+    '''makes a dict of avalible json files for study'''
     files =[]
     for file in os.listdir("./json"):
         files.append(file)
@@ -12,19 +13,19 @@ def list_files():
         files_dict[i+1] = file
     return(files_dict)
 
-def check_input(input):
-    try:
-        val = int(input)
-        print('wut')
-    except:
-        print("Invalid input, try again")
-        check_input(initialize())
-
-def initialize():
-    val = input(Fore.GREEN + Back.BLACK + "Please select a file number from the following options:\n" + Style.RESET_ALL + str(files).replace("{","").replace("}","") +"\n")
+def initialize(filez):
+    '''collects initial input from a user to determine which file to open'''
+    val = ""
+    val = input(Fore.GREEN + Back.BLACK + "Please select a file " + Fore.YELLOW + "number" + Fore.GREEN + " from the following options:\n" + Style.RESET_ALL + Fore.YELLOW + str(files).replace("{","").replace("}","") +"\n")
+   
+    while not val.isdigit() or int(val) > len(filez):
+        globals.clean()
+        val = input(Fore.RED + Back.BLACK + "INVALID ENTRY!! " + Fore.GREEN + "Try again:\n" + Style.RESET_ALL + Fore.YELLOW + str(filez).replace("{","").replace("}","") +"\n")
+   
     return val
+
+# flow
+
 files = list_files()
-
-# lets user to pick a file to study
-
-check_input(initialize())
+globals.clean()
+initialize(files)
