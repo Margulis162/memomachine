c8ee07ffe45c7e3b451bf695a4a4f4564f41fa09
Margulis162 <markmargulis@proton.me>
2024-06-14 07:23:58 -0400

    logic for reading json file and making a dict out of it

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import globals
+import json
 from colorama import init, Fore, Back, Style
 
 # _______f(x)____
@@ -16,16 +17,24 @@ def list_files():
 def initialize(filez):
     '''collects initial input from a user to determine which file to open'''
     val = ""
-    val = input(Fore.GREEN + Back.BLACK + "Please select a file " + Fore.YELLOW + "number" + Fore.GREEN + " from the following options:\n" + Style.RESET_ALL + Fore.YELLOW + str(files).replace("{","").replace("}","") +"\n")
+    val = input(Fore.GREEN + Back.BLACK + "Please select a file " + Fore.YELLOW + "number" + Fore.GREEN + " from the following options:\n" + Style.RESET_ALL + Fore.YELLOW + str(files).replace("{","").replace("}","") +"\n" + Style.RESET_ALL)
    
     while not val.isdigit() or int(val) > len(filez):
         globals.clean()
-        val = input(Fore.RED + Back.BLACK + "INVALID ENTRY!! " + Fore.GREEN + "Try again:\n" + Style.RESET_ALL + Fore.YELLOW + str(filez).replace("{","").replace("}","") +"\n")
+        val = input(Fore.RED + Back.BLACK + "INVALID ENTRY!! " + Fore.GREEN + "Try again:\n" + Style.RESET_ALL + Fore.YELLOW + str(filez).replace("{","").replace("}","") +"\n" + Style.RESET_ALL)
    
     return val
 
-# flow
-
+def ke
+# ___var___
 files = list_files()
+working_dict = {} 
+
+# flow
 globals.clean()
-initialize(files)
+
+# populates dictionary using selected file
+with open(f'./json/{files[int(initialize(files))]}', 'r') as f:
+    raw_data = f.read()
+    working_dict = json.loads(raw_data)
+
