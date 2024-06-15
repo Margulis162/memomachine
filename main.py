005b491a66d93e769115bb9d0d02ca8ce77f23d8
Margulis162 <markmargulis@proton.me>
2024-06-15 05:30:20 -0400

    some test formatting

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import os
 import json
 import random
+import shutil
 from colorama import init, Fore, Back, Style
 
 # _______f(x)____
 def clean():
     '''console cleaner'''
     os.system('cls' if os.name == 'nt' else 'clear')
+ 
 
 def list_files():
     '''makes a dict of avalible json files for study'''
@@ -18,16 +20,22 @@ def list_files():
         files_dict[i+1] = file
     return(files_dict)
 
+
 def initialize(filez):
     '''collects initial input from a user to determine which file to open'''
     val = ""
-    val = input(Fore.GREEN + Back.BLACK + "Please select a file " + Fore.YELLOW + "number" + Fore.GREEN + " from the following options:\n" + Style.RESET_ALL + Fore.YELLOW + str(files).replace("{","").replace("}","") +"\n" + Style.RESET_ALL)
-   
+    print(center_str.format(center = Fore.GREEN + "Please select a file " + Fore.YELLOW + "number" + Fore.GREEN + " from the following options:\n"))
+    print(center_str.format( center = Style.RESET_ALL + Fore.YELLOW + str(files).replace("{","").replace("}","") +"\n" + Style.RESET_ALL,))
+    val = input(center_str.format(center = ""))
+    
     while not val.isdigit() or int(val) > len(filez):
         clean()
-        val = input(Fore.RED + Back.BLACK + "INVALID ENTRY!! " + Fore.GREEN + "Try again:\n" + Style.RESET_ALL + Fore.YELLOW + str(filez).replace("{","").replace("}","") +"\n" + Fore.LIGHTWHITE_EX)
+        print( (Fore.RED + "INVALID ENTRY!! " + Fore.GREEN + "Try again:\n").center(screen_size))
+        print( Style.RESET_ALL + Fore.YELLOW + str(filez).replace("{","").replace("}","").center(screen_size))
+        val = input().center(screen_size)
    
     return val
+
 
 def main_loop():
     '''renders screens, sets logic'''
@@ -44,10 +52,12 @@ def main_loop():
             prev_answer = Fore.GREEN + Style.BRIGHT + prev_answ_update
     print(Fore.GREEN + "YOU MADE IT!")
 
+
 def whats_the_question():
     '''extracts the question var from the dictionary and makes it separate variable'''
     ask = working_dict["question"]
     del working_dict['question']
+
 
 # ___var___
 files = list_files()
@@ -55,6 +65,7 @@ ask=""
 working_dict = {} 
 key_nums = {}
 score = 0
+screen_size = shutil.get_terminal_size().columns
 #___flow___
 clean()
 
