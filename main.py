1484a07efb0b8e3a131a5b4955decf1c265eaea3
Margulis162 <markmargulis@proton.me>
2024-06-16 03:27:34 -0400

    everything is centered

--- main.py
+++ main.py
@@ -24,15 +24,18 @@ def list_files():
 def initialize(filez):
     '''collects initial input from a user to determine which file to open'''
     val = ""
-    print(center_str.format(center = Fore.GREEN + "Please select a file " + Fore.YELLOW + "number" + Fore.GREEN + " from the following options:\n"))
-    print(center_str.format( center = Style.RESET_ALL + Fore.YELLOW + str(files).replace("{","").replace("}","") +"\n" + Style.RESET_ALL,))
-    val = input(center_str.format(center = ""))
+    selection_str = Fore.GREEN + "Please select a file " + Fore.YELLOW + "number" + Fore.GREEN + " from the following options: "
+    files_str = Style.RESET_ALL + Fore.YELLOW + str(files).replace("{","").replace("}","") + Style.RESET_ALL
+    print(selection_str.center(screen_size))
+    print(files_str.center(screen_size))
+    val = input((screen_size//2 - 10) * " ")
     
     while not val.isdigit() or int(val) > len(filez):
         clean()
-        print( (Fore.RED + "INVALID ENTRY!! " + Fore.GREEN + "Try again:\n").center(screen_size))
-        print( Style.RESET_ALL + Fore.YELLOW + str(filez).replace("{","").replace("}","").center(screen_size))
-        val = input().center(screen_size)
+        invalid_str = Fore.RED + "INVALID ENTRY!! " + Fore.GREEN + "Try again:"
+        print( invalid_str.center(screen_size))
+        print( (Style.RESET_ALL + Fore.YELLOW + str(filez).replace("{","").replace("}","")).center(screen_size))
+        val = input((screen_size//2 - 10) * " ")
    
     return val
 
@@ -44,17 +47,30 @@ def main_loop():
         clean()
         question = random.choice(list(working_dict.keys()))
         prev_answ_update = working_dict[question]
-        print(f"The right answer for the previous question is: {prev_answer}")
-        answer = input(Fore.BLUE + Style.BRIGHT + ask + question + Fore.WHITE + Style.NORMAL +"\n").lower()
+
+        # rht_answ = "The right answer for the previous question is: " + prev_answer
+        rht_answ = "The right answer for the previous question is: " + prev_answer
+        qw_remains = Style.RESET_ALL + "There are " + Fore.YELLOW + str(len(working_dict))+ Style.RESET_ALL + " questions left"
+        
+        stat_bar = stat_str.format(left = rht_answ, right = qw_remains).center(screen_size)
+        underline = stat_str.format(left ="_" * 80, right ="_" * 80)
+        qw_str = Fore.BLUE + Style.BRIGHT + ask + Fore.GREEN + question + Fore.WHITE + Style.NORMAL 
+
+        print(stat_bar)
+        print(underline.center(screen_size) +"\n")
+        print(stat_str.format(left = "hello", right = 'Bye').center(screen_size))
+        answer = input(qw_str.center(screen_size) + "\n" + (screen_size//2 - 10) * " ").lower()
         prev_answer = Fore.RED + Style.BRIGHT + prev_answ_update
         if answer == working_dict[question]:
             del working_dict[question]
             prev_answer = Fore.GREEN + Style.BRIGHT + prev_answ_update
-    print(Fore.GREEN + "YOU MADE IT!")
+    end_str = Fore.GREEN + "YOU MADE IT!"
+    print(end_str.center(screen_size))
 
 
 def whats_the_question():
     '''extracts the question var from the dictionary and makes it separate variable'''
+    global ask
     ask = working_dict["question"]
     del working_dict['question']
 
@@ -66,6 +82,8 @@ working_dict = {}
 key_nums = {}
 score = 0
 screen_size = shutil.get_terminal_size().columns
+input_str ="{center:^20}"
+stat_str ="{left:<80}{right:>80}"
 #___flow___
 clean()
 
